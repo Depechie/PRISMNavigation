57952a62537f487e9bec97f10c162418d8122e22
Depechie <glenn.versweyveld@gmail.com>
2018-02-09 20:24:38 +0100

    Be sure to wrap menu pages inside a navigation page

--- BasePageDemo.NetStandard/ViewModels/MDViewModel.cs
+++ BasePageDemo.NetStandard/ViewModels/MDViewModel.cs
@@ -8,10 +8,10 @@ namespace BasePageDemo.NetStandard.ViewModels
     public class MDViewModel : ViewModelBase
     {
         private DelegateCommand _navigateVACommand;
-        public DelegateCommand NavigateVACommand => _navigateVACommand ?? (_navigateVACommand = new DelegateCommand(async () => await _navigationService.NavigateAsync("MainPage")));
+        public DelegateCommand NavigateVACommand => _navigateVACommand ?? (_navigateVACommand = new DelegateCommand(async () => await _navigationService.NavigateAsync("NavigationPage/MainPage")));
 
         private DelegateCommand _navigateVBCommand;
-        public DelegateCommand NavigateVBCommand => _navigateVBCommand ?? (_navigateVBCommand = new DelegateCommand(async () => await _navigationService.NavigateAsync("SecondPage")));
+        public DelegateCommand NavigateVBCommand => _navigateVBCommand ?? (_navigateVBCommand = new DelegateCommand(async () => await _navigationService.NavigateAsync("NavigationPage/SecondPage")));
 
         public MDViewModel(INavigationService navigationService) : base(navigationService)
         {
