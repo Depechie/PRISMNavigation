feb215ffbaa0a73f2268bf2812945ab68397b40b
Depechie <Glenn.Versweyveld@bankvanbreda.be>
2018-02-20 10:59:12 +0100

    Update xamarin forms nuget Start for PopupService

---
+++ BasePageDemo.NetStandard/Services/PopupService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Prism.Commands;
+
+namespace BasePageDemo.NetStandard.Services
+{
+    public class PopupService
+    {
+        public void DisplayPopup(string title, string content) //, DelegateCommand<PopupResult> command = null)
+        {}
+    }
+}
